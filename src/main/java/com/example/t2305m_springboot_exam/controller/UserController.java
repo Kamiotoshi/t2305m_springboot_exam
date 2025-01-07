0d41fe579d19c95235c6003dcdfed2bb31a2a78b
otoshi kami <kirito2932000@gmail.com>
2025-01-07 09:55:12 +0700

    first commit

---
+++ src/main/java/com/example/t2305m_springboot_exam/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.t2305m_springboot_exam.controller;
+
+import com.example.t2305m_springboot_exam.dto.req.UserReq;
+import com.example.t2305m_springboot_exam.dto.res.UserRes;
+import com.example.t2305m_springboot_exam.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/user")
+public class UserController {
+    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping()
+    public List<UserRes> getAllUser(){
+        return userService.all();
+    }
+    @PostMapping()
+    public ResponseEntity<UserRes> createUser(@RequestBody UserReq user){
+        return ResponseEntity.ok(userService.create(user));
+    }
+}
